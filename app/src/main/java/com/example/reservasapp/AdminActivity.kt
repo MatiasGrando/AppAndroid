687349533bee7040832dc3873d233c37166ab021
Matias Grando <matias.grando@hotmail.com>
2026-03-10 15:58:37 -0300

    Add Add menu action in admin panel

--- app/src/main/java/com/example/reservasapp/AdminActivity.kt
+++ app/src/main/java/com/example/reservasapp/AdminActivity.kt
@@ -1,11 +1,17 @@
 package com.example.reservasapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+
+        findViewById<Button>(R.id.btnAddMenuAdmin).setOnClickListener {
+            startActivity(Intent(this, AdminMenuActivity::class.java))
+        }
     }
 }
